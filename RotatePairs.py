88adadb89527c8d873994c179f0004a1701b850c
MrHadaward <111612923+MrHadaward@users.noreply.github.com>
2022-08-25 12:58:12 -0300

    Study Project

---
+++ RotatePairs.py
@@ -0,0 +1,34 @@
+#Objective of this project is to find all rotate pairs in Words.txt
+
+from itertools import count
+from time import sleep
+from tkinter import W
+
+
+txt_file = open('Words.txt').read().split()
+
+a_dict = {}
+
+for words in txt_file:
+    a_dict[words] = []
+
+for words in a_dict:
+    char = list(words)
+    f = words
+
+    for piece in char:
+        char.insert(0, char[-1])
+        del char[-1]
+        var = ''.join(char) 
+        
+        if var == f:
+            continue
+
+        elif var in a_dict:
+            a_dict[f] += [var]
+
+for keys in a_dict:
+    if a_dict[keys] != []:
+        print(f'{keys} : {a_dict[keys]}')
+
+            
