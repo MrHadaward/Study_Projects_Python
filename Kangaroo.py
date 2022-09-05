2d3214d8a993943193aff8b167742522a48912cd
MrHadaward <edu4rd0.b0rg3s@gmail.com>
2022-09-05 19:19:54 -0300

    Study Project

---
+++ Kangaroo.py
@@ -0,0 +1,19 @@
+class Kangaroo:
+
+    pouch = []
+
+    def __str__(self):
+        return ', '.join(self.pouch)
+
+    def __init__(self):
+        self.pouch = []
+
+    def put_in(self, object):
+        self.pouch.append(str(object))
+
+kanga = Kangaroo()
+kanga.put_in(1)
+
+roo = Kangaroo()
+roo.put_in(3)
+print(kanga, roo)
